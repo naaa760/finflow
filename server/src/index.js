98adc2c740e3f54f5b8304e7341ddaa54db385e7
nehhhhha6 <n0370525@gmail.com>
2025-02-26 17:25:02 +0530

    update

--- server/src/index.js
+++ server/src/index.js
@@ -17,6 +17,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Origin"],
     exposedHeaders: ["Content-Range", "X-Content-Range"],
